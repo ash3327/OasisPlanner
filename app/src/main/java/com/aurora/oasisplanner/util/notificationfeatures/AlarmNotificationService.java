868ca7678f81df57f0c590d05e9e923d5e923d06
kht <samkht3327@gmail.com>
2023-09-02 23:03:17 +0800

    Notification Ui Update

--- app/src/main/java/com/aurora/oasisplanner/util/notificationfeatures/AlarmNotificationService.java
+++ app/src/main/java/com/aurora/oasisplanner/util/notificationfeatures/AlarmNotificationService.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 
 import androidx.core.app.NotificationCompat;
 
@@ -52,6 +53,7 @@ public class AlarmNotificationService {
                         new NotificationCompat.BigTextStyle()
                                 .bigText(alarm.getContents(true))
                 )
+                //.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.g_regular))
                 .setContentIntent(pendingIntent)
                 /*.addAction(
                         R.drawable.menuic_sprout,
