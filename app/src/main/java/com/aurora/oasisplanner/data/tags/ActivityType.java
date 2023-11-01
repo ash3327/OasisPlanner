ed9cc8237071889a48fec3123c85a1e1d5740358
kht <samkht3327@gmail.com>
2023-11-01 11:33:05 +0800

    Alarm Update: Fixed Inconsistency Bug ---------------------- The old ActivityType.Type enum value "group" was changed to "activity", but the old data was not updated to match this change. This problem is now fixed.

--- app/src/main/java/com/aurora/oasisplanner/data/tags/ActivityType.java
+++ app/src/main/java/com/aurora/oasisplanner/data/tags/ActivityType.java
@@ -3,7 +3,9 @@ package com.aurora.oasisplanner.data.tags;
 import java.util.regex.Pattern;
 
 public class ActivityType {
-    public enum Type {gap, activity, doc};
+    public enum Type {
+        gap, activity, doc
+    };
 
     private static String SEP = "$$";
 
@@ -16,6 +18,7 @@ public class ActivityType {
     }
     public static ActivityType valueOf(String str) {
         String[] li = str.split(Pattern.quote(SEP));
+        li[0] = li[0].replaceAll("group", "activity");
         return new ActivityType(Type.valueOf(li[0]), Integer.parseInt(li[1]));
     }
     public String toString() {
