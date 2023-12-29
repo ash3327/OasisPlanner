5930fc9ed0097018c1680a5ff42a91ac6552e010
kht <samkht3327@gmail.com>
2023-12-29 15:34:01 +0800

    Memo System Update: Quick memos are now displayed in the Memos fragment. ---------------------- Unstable non-final version. No bugs found yet.

--- app/src/main/java/com/aurora/oasisplanner/fragments/MemosFragment.java
+++ app/src/main/java/com/aurora/oasisplanner/fragments/MemosFragment.java
@@ -7,21 +7,50 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.aurora.oasisplanner.R;
 import com.aurora.oasisplanner.activities.MainActivity;
 import com.aurora.oasisplanner.data.tags.Page;
+import com.aurora.oasisplanner.databinding.MemoFragmentBinding;
+import com.aurora.oasisplanner.presentation.ui.memos.MemoViewModel;
+import com.aurora.oasisplanner.presentation.ui.memos.components.MemosAdapter;
 
 public class MemosFragment extends Fragment {
 
+    private MemoFragmentBinding binding;
+    private MemoViewModel memosViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_memos, container, false);
-        MainActivity.page = Page.MEMOS;
+        binding = MemoFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        navigate();
         return root;
     }
 
     public static Page currentPage = Page.MEMOS;
+
+    public void navigate() {
+        MainActivity.page = Page.MEMOS;
+        currentPage = Page.MEMOS;
+
+        RecyclerView recyclerView = binding.boxList;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+
+        final MemosAdapter adapter = new MemosAdapter(recyclerView);
+        adapter.setOnChangeListener(
+                (size)-> binding.textHome.setVisibility(size == 0 ? View.VISIBLE : View.INVISIBLE)
+        );
+        recyclerView.setAdapter(adapter);
+
+        memosViewModel = new ViewModelProvider(this).get(MemoViewModel.class);
+        memosViewModel.getMemos().observe(getViewLifecycleOwner(), adapter::setMemos);
+    }
+
     @Override
     public void onResume() {
         super.onResume();
