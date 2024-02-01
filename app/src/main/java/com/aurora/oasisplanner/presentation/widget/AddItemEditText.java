22333584d69927567459aa1236d3e26723e0f342
kht <samkht3327@gmail.com>
2024-02-01 21:46:15 +0800

    EditAgenda Update: Warn users when they attempt to input a long title. -------------------------------------- Takes no action to prohibit them from doing so.

--- app/src/main/java/com/aurora/oasisplanner/presentation/widget/AddItemEditText.java
+++ app/src/main/java/com/aurora/oasisplanner/presentation/widget/AddItemEditText.java
@@ -1,21 +1,32 @@
 package com.aurora.oasisplanner.presentation.widget;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 
+import com.aurora.oasisplanner.R;
 import com.aurora.oasisplanner.databinding.AddItemEditTextBinding;
+import com.aurora.oasisplanner.util.styling.Resources;
+import com.aurora.oasisplanner.util.styling.Styles;
 
 public class AddItemEditText extends LinearLayout {
     private final ImageView icon;
     private final EditText editText;
     private final AddItemEditTextBinding binding;
+    private final View root;
+    private final TextView warningTv;
 
     public AddItemEditText(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -23,14 +34,39 @@ public class AddItemEditText extends LinearLayout {
         binding = AddItemEditTextBinding.inflate(inflater, this, true);
         icon = binding.aietIcon;
         editText = binding.aietEditText;
+        warningTv = binding.aietEditWarning;
+        root = binding.getRoot();
     }
 
     public ImageView getIcon() { return icon; }
     public EditText getEditText() { return editText; }
 
+    public @StringRes int inputWarnings(CharSequence s) {
+        if (Styles.getLength(s.toString()) > 13)
+            return R.string.warning_input_too_long;
+        return -1;
+    }
     public void setOnEnterListener(OnEnterListener r) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                @StringRes int warnings = inputWarnings(s);
+                if (warnings != -1) {
+                    root.setBackgroundColor(Resources.getColor(R.color.warning_color));
+                    warningTv.setVisibility(VISIBLE);
+                    warningTv.setText(warnings);
+                } else {
+                    root.setBackgroundColor(Color.TRANSPARENT);
+                    warningTv.setVisibility(GONE);
+                }
+            }
+        });
         editText.setOnKeyListener((view,keyCode,keyEvent)->{
             String s = editText.getText().toString().trim();
+
             if (keyCode == KeyEvent.KEYCODE_ENTER &&
                     keyEvent.getAction() == KeyEvent.ACTION_UP
             ) {
